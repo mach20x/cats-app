f674901d6e028f2ceb0a0d5212858be41f52e89d
Michael Park <mach20x@yahoo.com>
2017-11-03 14:34:54 -0400

    added HTTParty3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index' => 'home#index'
 
-  #manual entry for test-branch-raul
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
