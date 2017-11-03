f22e0139480113dea6f4b51da185c9438df724ff
Michael Park <mach20x@yahoo.com>
2017-11-03 13:26:02 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  #manual entry for test-branch-raul
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
